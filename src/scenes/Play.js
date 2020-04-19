ab96c819cf7b6e24615a34eb11826686a0ea7795
Kiara Yupangco <kyupangc@ucsc.edu>
2020-04-19 00:42:43 -0700

    Initial Commit

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -22,8 +22,10 @@ class Play extends Phaser.Scene {
         this.add.rectangle(5, 443, 630, 32, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(5, 5, 32, 455, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(603, 5, 32, 455, 0xFFFFFF).setOrigin(0, 0);
+        /*
         // new UI background
         this.add.rectangle(37, 42, 566, 64, 0x6F4BCF).setOrigin(0, 0);
+        */
 
         // add rocket (p1)
         this.p1Rocket = new Rocket(this, game.config.width/2 - 8, 431, 'rocket').setScale(0.5, 0.5).setOrigin(0, 0);
@@ -55,8 +57,8 @@ class Play extends Phaser.Scene {
         let scoreConfig = {
             fontFamily: 'Courier',
             fontSize: '28px',
-            backgroundColor: '#5BC0F2',
-            color: '#20176A',
+            ///backgroundColor: '#5BC0F2',
+            color: '#FFFFFF',
             align: 'right',
             padding: {
                 top: 5,
@@ -70,8 +72,8 @@ class Play extends Phaser.Scene {
         let fireText = {
             fontFamily: 'Courier',
             fontSize: '28px',
-            backgroundColor: '#5BC0F2',
-            color: '#20176A',
+            //backgroundColor: '#5BC0F2',
+            color: '#FFFFFF',
             align: 'right',
             padding: {
                 top: 5,
