62d3b826750e70b558d621ece12041e5229e4492
Kiara Yupangco <kyupangc@ucsc.edu>
2020-04-19 00:45:32 -0700

    Initial Commit

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -66,8 +66,8 @@ class Play extends Phaser.Scene {
             },
             fixedWidth: 100
         }
-        this.add.text(25, 54, 'SCORE: ', scoreConfig);
-        this.scoreLeft = this.add.text(69, 54, this.p1Score, scoreConfig);
+        this.add.text(35, 54, 'SCORE: ', scoreConfig);
+        this.scoreLeft = this.add.text(79, 54, this.p1Score, scoreConfig);
 
         // FIRE UI display
         let fireText = {
