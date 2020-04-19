bbad1bab3b593517d8e39d8b4576a8fb334a965a
Kiara Yupangco <kyupangc@ucsc.edu>
2020-04-18 23:32:19 -0700

    Initial Commit

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -13,7 +13,7 @@ class Menu extends Phaser.Scene {
     create() {
         // menu display
         let menuConfig = {
-            fontFamily: 'Roboto Condensed',
+            fontFamily: 'Tahoma',
             fontSize: '28px',
             backgroundColor: '#F3B141',
             color: '#843605',
