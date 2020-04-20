5982b61a70bb352e005f73a7c681e026139a5307
Kiara Yupangco <kyupangc@ucsc.edu>
2020-04-19 17:24:34 -0700

    Initial Commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
-/* POINTS BREAKDOWN
+/* ROCKET PATROL Modded (by Kiara Yupangco)
+   POINTS BREAKDOWN (with explanation for some of the options)
    -Create a new scrolling tile sprite for the background (10)
+        -I want the scrolling background to be more interesting, so adding a gradient and space-themed props makes the game have more feel
    -Implement the 'FIRE' UI text from the original game (10)
    -Create a new title screen (15)
+        -Again with the scrolling tile sprite, I feel that the original background feels empty (also I remove the text borders to see the title background more)
    -Replace the UI borders with new artwork (15)
+        -Decided to go with a Gameboy Color-esque border (by adding them as images after the blocks of code for rocket and spaceship in order to overlap them)
    -Create new artwork for all of the in-game assets (rocket, spaceships, explosion) (25)
    -Create a new spaceship type that's smaller, moves faster, and is worth more points (25)
+        -The smaller spaceship is worth 69 points (nice)
 */
 
 let config = {
