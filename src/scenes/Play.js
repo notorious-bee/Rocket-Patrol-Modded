8fd5e46e3fe5f288acedd60cb9290b702cf5c8a9
Kiara Yupangco <kyupangc@ucsc.edu>
2020-05-01 22:19:40 -0700

    Initial Commit

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -25,7 +25,7 @@ class Play extends Phaser.Scene {
 
     create() {
         // place tile sprite
-        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
+        //this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
 
         //test
         this.crab = this.add.sprite(200,200,'crab');
